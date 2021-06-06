b20caea6fa81491208a03e267be9d2a706c027ad
timor.rust <75886852+timorust@users.noreply.github.com>
2021-06-06 12:02:03 +0300

    copy from jph to firestr

--- src/app/pages/blog-page/blog-page.component.ts
+++ src/app/pages/blog-page/blog-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {PostInterface} from "../../interfaces/post.interface";
 import {CoreService} from "../../services/core.service";
+import {UserInterface} from "../../interfaces/user.interface";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'ab-blog-page',
@@ -11,14 +13,31 @@ export class BlogPageComponent implements OnInit {
 
 
   blogPosts: PostInterface[];
-  constructor(private coreService: CoreService) { }
+
+  user: UserInterface;
+  userSub;
+  constructor(private coreService: CoreService,
+              private authS: AuthService) { }
 
   ngOnInit(): void {
-    this.getBlogPost();
+    this.userSub = this.authS.user.subscribe((user: UserInterface) => {
+      this.user = user;
+      this.getBlogPost();
+    });
+
   }
+
   getBlogPost() {
-    this.coreService.getBlogPosts().subscribe((postItem: PostInterface[]) => {
-      this.blogPosts = postItem;
+    this.coreService.getBlogPosts().subscribe((blogPosts: PostInterface[]) => {
+      this.blogPosts = blogPosts;
+
+      blogPosts.forEach(blogItem => {
+        blogItem.userId = this.user.uid;
+        this.coreService.copyData(blogItem).then(() => {
+
+        })
+
+      })
     })
   }
 
--- src/app/services/core.service.ts
+++ src/app/services/core.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {AngularFirestore} from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoreService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+              private anf: AngularFirestore) { }
 
   getBlogPosts() {
     return this.httpClient.get(`https://jsonplaceholder.typicode.com/posts`);
+  }
+  copyData(blogItem) {
+    return this.anf.doc(`posts/${blogItem.id}`).set({...blogItem});
   }
 }
